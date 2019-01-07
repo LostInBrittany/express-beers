fa31fb4ac664cba09ebe961dbce2d15e76a31519
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2019-01-07 11:04:21 +0100

    Fixing for Mongodb 3.x

--- step-05/index.js
+++ step-05/index.js
@@ -3,6 +3,7 @@ var app = express();
 var assert = require('assert');
 var MongoClient = require('mongodb').MongoClient;
 
+
 var findBeer = function(db, beerId, callback) {
    var cursor =db.collection('beers').find({id: beerId} );
    var beer;
@@ -31,12 +32,13 @@ var findBeers = function(db, beerList,  callback) {
 app.get('/beers', function (req, res) {
   console.log('Received request for beers from', req.ip)
 
-  MongoClient.connect(url, function(err, db) {
+  MongoClient.connect(url, function(err, client) {
+    const db = client.db(dbName);
     assert.equal(null, err);
     var beerList = [];
     findBeers(db, beerList, function() {
       res.json(beerList);
-      db.close();
+      client.close();
     });
 
   });
@@ -44,12 +46,13 @@ app.get('/beers', function (req, res) {
 
 app.get('/beer/:beerId', function (req, res) {
   console.log('Received request for '+req.param('beerId')+' from', req.ip)
-  MongoClient.connect(url, function(err, db) {
+  MongoClient.connect(url, function(err, client) {
+    const db = client.db(dbName);
     assert.equal(null, err);
     findBeer(db, req.param('beerId'),  function(beer) {
       console.log(beer)
       res.json(beer);
-      db.close();
+      client.close();
     });
 
   });
@@ -60,7 +63,8 @@ app.use('/beers/img', express.static('img'));
 app.use('/img', express.static('img'));
 app.use(express.static('public'));
 
-var url = 'mongodb://localhost:27017/test';
+var url = 'mongodb://localhost:27017';
+var dbName = 'test'
 
 
 
