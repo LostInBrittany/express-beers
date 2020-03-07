2c83188c6f75e9f202748e0ebc799e34c0104e6c
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2020-03-07 07:38:33 +0100

    Variable PORT

--- step-05/index.js
+++ step-05/index.js
@@ -45,7 +45,7 @@ var dbName = 'test'
 
 
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Listening at http://%s:%s', host, port);
